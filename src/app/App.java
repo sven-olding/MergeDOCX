5f7ccfea459df859e1d8fabd74eb1604d846cadb
Sven Olding <sven.olding@gmx.de>
2020-03-24 16:11:54 +0100

    initial commit

---
+++ src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      XWPFDocument wordDoc = new XWPFDocument(sourceFile);
+    }
+}
